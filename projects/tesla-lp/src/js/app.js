0b3028820bd101a0e70f1ecc0b4713a65c6f86d9
Volodymyr Mazurets <mazurets@Volodymyrs-MacBook-Pro.local>
2020-09-14 21:35:31 +0300

    📱 tesla responsive added

--- projects/tesla-lp/src/js/app.js
+++ projects/tesla-lp/src/js/app.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("menu");
 const closeBtn = document.getElementById("close");
 const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
 
 const eventListenerMenuCreator = (elenemt, display) => {
   elenemt.addEventListener("click", (e) => {
@@ -11,3 +12,10 @@ const eventListenerMenuCreator = (elenemt, display) => {
 
 eventListenerMenuCreator(burger, 'flex');
 eventListenerMenuCreator(closeBtn, 'none');
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset < nav.clientHeight) {
+      nav.classList.remove('nav--filled');
+
+  } else nav.classList.add("nav--filled");
+});
