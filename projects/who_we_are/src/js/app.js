1472928172f67db369760b6c58cd2b87706b2c22
Volodymyr Mazurets <mazurets@Volodymyrs-MacBook-Pro.local>
2020-09-21 20:56:25 +0300

    ✨ who we are template added

---
+++ projects/who_we_are/src/js/app.js
@@ -0,0 +1,25 @@
+const list = document.getElementById('list');
+
+const DATA = [
+    {
+        image: 'https://picsum.photos/id/237/200/300',
+        full_name: 'John Hodson',
+        facebook: '',
+        twitter: '',
+        dribble: '',
+    },
+    {
+        image: 'https://picsum.photos/id/238/200/300',
+        full_name: 'Volodymyr Mazurets',
+        facebook: '',
+        twitter: '',
+        dribble: '',
+    },
+    {
+        image: 'https://picsum.photos/id/239/200/300',
+        full_name: 'Sahsa Sample',
+        facebook: '',
+        twitter: '',
+        dribble: '',
+    }
+]; 
